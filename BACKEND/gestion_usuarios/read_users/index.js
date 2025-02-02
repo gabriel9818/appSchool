f65bd31edf5f831f5faf0d665df3b726ba3a0fe8
gabriel9818 <garbielruales18@gmail.com>
2025-02-02 15:05:04 -0500

    feat: :bug: deploy bug

--- BACKEND/gestion_usuarios/read_users/index.js
+++ BACKEND/gestion_usuarios/read_users/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001; // Usa un puerto diferente al de create_users
 
-// Middleware
+// Middleware s
 app.use(bodyParser.json());
 
 // Rutas
