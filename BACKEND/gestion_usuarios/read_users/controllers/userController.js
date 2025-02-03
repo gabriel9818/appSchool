c8d49affa9406faa6bd9b7717949ff880c6ca14f
gabriel9818 <garbielruales18@gmail.com>
2025-02-02 23:29:06 -0500

    feat: :rocket: test deploy read users

--- BACKEND/gestion_usuarios/read_users/controllers/userController.js
+++ BACKEND/gestion_usuarios/read_users/controllers/userController.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.findAll(); // Obtiene todos los usuarios
+    const users = await User.findAll(); // get users
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -12,7 +12,7 @@ const getAllUsers = async (req, res) => {
 const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findByPk(id); // Busca un usuario por su ID
+    const user = await User.findByPk(id); // search users for ID
 
     if (!user) {
       return res.status(404).json({ error: 'Usuario no encontrado' });
