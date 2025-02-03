82dce2abb23c8f723f4a479c957aa1249b4523fd
gabriel9818 <garbielruales18@gmail.com>
2025-02-03 12:38:40 -0500

    feat: :construction_worker: add yml for deletes users and test deploy with actions

--- BACKEND/gestion_usuarios/delete_users/index.js
+++ BACKEND/gestion_usuarios/delete_users/index.js
@@ -20,7 +20,7 @@ app.use('/api/delete', userRoutes);
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected successfully.');
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
