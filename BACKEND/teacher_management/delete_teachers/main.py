19c5fef5eeb424713aeb359c874f171489b4f620
gabriel9818 <garbielruales18@gmail.com>
2025-02-11 12:30:54 -0500

    feat: :bug: test deploy teacher 2

--- BACKEND/teacher_management/delete_teachers/main.py
+++ BACKEND/teacher_management/delete_teachers/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes.teacher_delete_routes import router
 from db.database import Base, engine
 
-# Crear las tablas si no existen
+# created tables if no exists
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
