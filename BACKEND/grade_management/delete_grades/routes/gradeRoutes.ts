ceddd85050efadc5a431849d42058777f4c8ce4f
gabriel9818 <garbielruales18@gmail.com>
2025-02-13 01:41:22 -0500

    feat: :bug: test deploy

--- BACKEND/grade_management/delete_grades/routes/gradeRoutes.ts
+++ BACKEND/grade_management/delete_grades/routes/gradeRoutes.ts
@@ -3,7 +3,8 @@ import { deleteGrade } from "../controllers/gradeDeleteController.js";
 
 const router = Router();
 
-// 💡 Usa el método `delete` en vez de `post
+// 💡 Usa el método `delete` en vez de `post'
+
 router.delete("/grades/:id", (req, res) => deleteGrade(req, res));
 
 export default router;
