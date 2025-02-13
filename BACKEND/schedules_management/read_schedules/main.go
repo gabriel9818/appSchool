93ae939ce0c368c40a0ebbbe52521f00fd2c1562
gabriel9818 <garbielruales18@gmail.com>
2025-02-12 21:10:59 -0500

    feat: :bug: test deploy

--- BACKEND/schedules_management/read_schedules/main.go
+++ BACKEND/schedules_management/read_schedules/main.go
@@ -5,22 +5,29 @@ import (
 	"net/http"
 	"os"
 
-	"read_schedules/db"
-	"read_schedules/routes"
+	"created_schedules/db"
+	"created_schedules/routes"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	// Si las variables de entorno ya están definidas, omitir cargar `.env`
+	if os.Getenv("DB_HOST") == "" {
+		log.Println("No environment variables found, loading .env file...")
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
 	}
 
+	// Conexión a la base de datos
 	db.Connect()
 
+	// Configurar rutas
 	router := routes.SetupRoutes()
 
+	// Iniciar el servidor
 	port := os.Getenv("API_PORT")
 	if port == "" {
 		port = "8081"
